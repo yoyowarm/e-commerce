031c05138a1e643268dde323a4cbc2f1c9a29078
yoyo <po8867@gmail.com>
2020-12-02 07:47:18 +0800

    修改登出樣式 個人資訊切版

--- oinvip_web_app/src/views/MyPage/components/logout.vue
+++ oinvip_web_app/src/views/MyPage/components/logout.vue
@@ -5,16 +5,16 @@
   :close-on-click-modal="false"
   :close-on-press-escape="false"
   top="60%"
-  width="70%"
+  width="80%"
   custom-class="logout"
 >
 <div class="container">
   <img src="@/assets/images/logout_confirm.svg" alt="">
-  <span>这是一段信息</span>
-  <span slot="footer" class="dialog-footer">
-    <el-button @click="cancel">取 消</el-button>
-    <el-button type="primary" @click="confirm">确 定</el-button>
-  </span>
+  <span>忍心登出嗎？</span>
+  <div slot="footer" class="dialog-footer">
+    <el-button @click="confirm" class="confirm" round>登出</el-button>
+    <el-button type="primary" @click="cancel" round>繼續留下</el-button>
+  </div>
 </div>
 </el-dialog>
 </template>
@@ -46,11 +46,27 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    padding: 0px
   }
   img {
     width: 68px;
+    margin-bottom: 15px
+  }
+  .confirm {
+      background: #F0F0ED;
+      border: 0px
+  }
+  button {
+      width: 125px;
+  }
+  .dialog-footer {
+    margin-top: 25px;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
   }
 }
+
 /deep/ .el-dialog {
   border-radius: 20px!important;
 }
--- oinvip_web_app/src/views/MyPage/components/user-info.vue
+++ oinvip_web_app/src/views/MyPage/components/user-info.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="user-info flex is-vertical">
-    <el-row type="flex" justify="center">
-      <el-col :span="6"></el-col>
-      <el-col :span="18"></el-col>
+  <div class="user-info ">
+    <el-row type="flex" justify="center" class="h-100 w-100">
+      <el-col :span="7">
+        <img src="@/assets/images/avatar.svg" alt="">
+      </el-col>
+      <el-col :span="17">
+        <ul class="account">
+          <li>User name</li>
+          <li><span>會員：user1234</span></li>
+          <li><el-button class="edit" round>編輯我的資料</el-button></li>
+        </ul>
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -15,8 +23,23 @@ export default {
 
 <style lang="less" scoped>
 .user-info {
-  height: 200px;
+  height: 169px;
   width: 100%;
-  background: #eee;
+  padding: 35px 30px;
+  display: flex;
+  align-items: center;;
+  background: url("../../../assets/images/user-info-bg.svg")no-repeat;
+  img {
+    width: 100px;
+  };
+  .account {
+    padding-left: 30px;
+    .edit {
+      background: #A9A29B;
+      color: #fff;
+      padding: 5px 12px;
+      font-size: 14px;
+    }
+  }
 }
 </style>
