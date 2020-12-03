3b4d7c5353dba94490ca95963e6cee3ffa7a547e
yoyowarmli <yoyowarmli@jetfuel.com.tw>
2020-12-03 17:55:36 +0800

    編輯個人資料 新增用戶大頭照區塊

--- oinvip_web_app/src/views/MyPage/editProfile/index.vue
+++ oinvip_web_app/src/views/MyPage/editProfile/index.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>編輯個人資料</div>
+  <div>
+    <custom-card class="user-block">
+      <div class="avatar">
+        <img src="@/assets/images/avatar.svg" alt="">
+      </div>
+    </custom-card>
+    <custom-card>
+      s
+    </custom-card>
+  </div>
 </template>
 
 <script>
+import customCard from '@/components/custom-card'
 export default {
-
+  components: {
+    customCard
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.user-block {
+  position: relative;
+  height: 169px;
+  width: 100%;
+  background: url("../../../assets/images/user-info-bg.svg")no-repeat;
+  .avatar {
+    width: 100px;
+    height: 100px;
+    margin: 0 auto;
+    margin-top: 10px;
+    img {
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+}
 </style>
