d3ab3692d1c220ebedb954681d366327b72b8b71
顏巨鴻 <hugo.c.yan@7officer.com>
2020-12-14 16:33:45 +0800

    no message

---
+++ src/views/gift/Branch.vue
@@ -0,0 +1,27 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Branch",
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  mounted() {
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
